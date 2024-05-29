b03bfa2caebc9e0728d75b77701d1926acf792ba
ofey_debian23 <domenuk10@gmail.com>
2024-05-29 16:40:46 +0300

    fixing

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,6 +2,7 @@ def main():
     text = '''poetry run python -m brain_games.scripts.brain_games
 Welcome to the Brain Games!'''
     print(text)
+    additional_info = ""
 
 if __name__ == "__main__":
     main()
