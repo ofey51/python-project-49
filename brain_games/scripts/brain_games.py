513f7663125c43f1e99ac95104740bf316eda017
ofey_debian23 <domenuk10@gmail.com>
2024-06-01 02:06:58 +0300

    hello Tition!

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,9 +3,10 @@ from brain_games.cli import welcome_user
 
 
 def main():
-    text = '''Welcome to the Brain Games!'''
-    print(text)
-    welcome_user()
+    # text = '''Welcome to the Brain Games!'''
+    # print(text)
+    # welcome_user()
+    print("Hello, Tirion!")
 
 
 if __name__ == "__main__":
